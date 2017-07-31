8ec10b7667a3c3a30a7b943f2720c571572d0dad
Messidt <piotrekksz@gmail.com>
2017-07-31 11:38:46 +0200

    Added events to buttons in order to change photos. Autoslide stops when user changes photo manually.

--- script/main.js
+++ script/main.js
@@ -1,7 +1,15 @@
 window.onload = function() {
     
     let imgNumber = Math.floor(Math.random() * 5) + 1;
+
+    const slider = document.getElementById('slider');
+
+    // Catching control buttons //
+    const previous = document.getElementById('previous');
+
+    const next = document.getElementById('next');
     
+    // Changing photos automatically //
     function changePhoto() {
         
         imgNumber++;
@@ -15,6 +23,41 @@ window.onload = function() {
         slider.innerHTML = file;
     }
     
-    setInterval(changePhoto, 4000);
+    
+    // Changing to the next photo (auto change is stopped) //
+    function nextSlide() {
+
+        clearInterval(startSliding);
+        
+        imgNumber++;
+
+         if (imgNumber > 5) imgNumber = 1; 
+
+        let file = '<img src="images/img' + imgNumber + '.jpg" />';
+
+        slider.innerHTML = file;
+    }
+
+    // Changing to the previous photo (auto change is stopped) //
+    function prevSlide() {
+        
+        clearInterval(starSliding);
+        
+        imgNumber--;
+
+        if (imgNumber < 1) imgNumber = 5;
+
+        let file = '<img src="images/img' + imgNumber + '.jpg" />';
+
+        slider.innerHTML = file;
+    }
+    
+    const startSliding = setInterval(changePhoto, 3000);
+
+    // Events for buttons //
+    previous.onclick = prevSlide;
+    next.onclick = nextSlide;
+
+
     
 }
